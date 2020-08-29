a722316d97ef1cf3178bda8834151b6a06a75ba0
Jatin <64397570+jatin-groot@users.noreply.github.com>
2020-08-30 01:47:10 +0530

    Added Firebase Database

--- Hooks/src/components/Ingredients/Ingredients.js
+++ Hooks/src/components/Ingredients/Ingredients.js
@@ -1,30 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import { tryStatement } from "@babel/types";
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientsHandler = newIngredient => {
+  const addIngredientsHandler = async newIngredient => {
     if (newIngredient) {
-      setIngredients(previousIngredients => [
-        ...previousIngredients,
-        { id: Math.random(), ...newIngredient }
-      ]);
+      try {
+        const response = await fetch(
+          "https://covid-c1962.firebaseio.com/ingredients.json",
+          {
+            method: "Post",
+            body: JSON.stringify(newIngredient),
+            headers: {
+              "Content-type": "application/json"
+            }
+          }
+        );
+        const res = await response.json();
+        setIngredients(previousIngredients => [
+          ...previousIngredients,
+          { id: res.name, ...newIngredient }
+        ]);
+      } catch (error) {
+        console.log("error", error.message);
+      }
     }
   };
 
-  const removeHandler = (event, ingredientId) => {
+  const removeHandler = ingredientId => {
     if (ingredientId) {
+      const deletedIngredient = ingredients.find(
+        ingredient => ingredient.id === ingredientId
+      );
+
+      if (deletedIngredient) {
+        setIngredients(previousIngredients =>
+          previousIngredients.filter(
+            ingredient => ingredient.id !== ingredientId
+          )
+        );
+      }
     }
   };
-  console.log("ingredients", ingredients);
+
+  const fetchIngredients = async () => {
+    try {
+      const response = await fetch(
+        "https://covid-c1962.firebaseio.com/ingredients.json"
+      );
+      let fetchedIngredients = await response.json();
+      const ingredientsArr = [];
+      fetchedIngredients = Object.entries(fetchedIngredients);
+      fetchedIngredients.forEach(([id, value]) =>
+        ingredientsArr.push({ id, ...value })
+      );
+      setIngredients(ingredientsArr);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
   return (
     <div className="App">
       <IngredientForm addIngredientsHandler={addIngredientsHandler} />
-
       <section>
         <Search />
         <IngredientList
